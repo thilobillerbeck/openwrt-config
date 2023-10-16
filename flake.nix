18737d25b7f6f365245af3a1c1be57114fff85dd
Thilo Billerbeck <thilo.billerbeck@officerent.de>
2023-10-17 01:35:03 +0200

    first commit

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  inputs = {
+    openwrt-imagebuilder.url = "github:astro/nix-openwrt-imagebuilder";
+  };
+  outputs = { self, nixpkgs, openwrt-imagebuilder }: {
+    packages.x86_64-linux.default =
+      let
+        pkgs = nixpkgs.legacyPackages.x86_64-linux;
+
+        profiles = openwrt-imagebuilder.lib.profiles { inherit pkgs; };
+
+        config = profiles.identifyProfile "asus_tuf-ax4200" // {
+          packages = [ "umdns" "luci" "luci-app-firewall" "luci-app-sqm" "sqm-scripts" "luci-proto-wireguard" "luci-app-wireguard" ];
+        };
+      in
+      openwrt-imagebuilder.lib.build config;
+  };
+}
