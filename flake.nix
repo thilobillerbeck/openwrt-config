2648d3b93141ae7cccbf3383651c3cf81555d137
Thilo Billerbeck <thilo.billerbeck@officerent.de>
2025-03-10 01:53:31 +0100

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
         profiles = openwrt-imagebuilder.lib.profiles { inherit pkgs; };
 
         config = profiles.identifyProfile "asus_tuf-ax4200" // {
-          packages = [ "umdns" "luci" "luci-app-firewall" "luci-app-sqm" "sqm-scripts" "luci-proto-wireguard" "luci-app-wireguard" "pbr" "luci-app-pbr" "dnsmasq-full" ];
+          packages = [ "umdns" "luci" "luci-app-firewall" "luci-app-sqm" "sqm-scripts" "luci-proto-wireguard" "pbr" "luci-app-pbr" "dnsmasq" ];
         };
       in
       openwrt-imagebuilder.lib.build config;
